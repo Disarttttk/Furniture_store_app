d89997344ad22bc8af65eae8603dde961143dba3
Disarttk <mmmm@google.com>
2024-05-10 22:57:43 +0300

    Создать urls.py в приложении main, в app/urls.py маршрутизация через include

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from main.views import index, about
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('about/', about, name='about'),
+
+]
