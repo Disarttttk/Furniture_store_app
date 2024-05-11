51b03a2bfab9837dd30bb63edb18775c72b6add2
Disarttk <mmmm@google.com>
2024-05-11 17:41:39 +0300

    Добавить приложение goods и подключить файлы catalog.html и product.html

---
+++ goods/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def catalog(request):
+    return render(request, template_name='goods/catalog.html')
+
+
+def product(request):
+    return render(request, template_name='goods/product.html')
