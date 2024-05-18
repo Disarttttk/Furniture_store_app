fc07b8eb4b6074f06e6e0b67b39f83bca5fb8cc4
Disarttk <mmmm@google.com>
2024-05-18 13:26:58 +0300

    Заинклудить все шаблоны корзины

--- users/views.py
+++ users/views.py
@@ -67,6 +67,10 @@ def profile(request):
     return render(request, template_name='users/profile.html', context=context)
 
 
+def users_cart(request):
+    return render(request, 'users/users-cart.html')
+
+
 @login_required
 def logout(request):
     messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
