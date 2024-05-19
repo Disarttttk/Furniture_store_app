d55b76628264960111ca2537f10a8a0376902d3a
Disarttk <mmmm@google.com>
2024-05-19 15:44:45 +0300

    Сессии корзин для анонимного пользователя

--- carts/models.py
+++ carts/models.py
@@ -16,7 +16,6 @@ class CartQuerySet(models.QuerySet):
 
 
 class Cart(models.Model):
-
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Пользователь')
     product = models.ForeignKey(to=Products, on_delete=models.CASCADE, verbose_name='Товар')
     quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Количество')
@@ -34,4 +33,4 @@ class Cart(models.Model):
         return round(self.product.sell_price() * self.quantity, 2)
 
     def __str__(self):
-        return f'Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}'
+        return f'Корзина {self.user} | Товар {self.product.name} | Количество {self.quantity}'
